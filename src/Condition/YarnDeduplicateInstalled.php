7c792b3e894254364377cd2867a43aba83c1fb0d
Arnout Boks <arnout.boks@moxio.com>
2019-12-23 10:36:12 +0100

    Add condition to check if yarn-deduplicate is installed

---
+++ src/Condition/YarnDeduplicateInstalled.php
@@ -0,0 +1,15 @@
+<?php
+namespace Moxio\CaptainHook\YarnDeduplicate\Condition;
+
+use CaptainHook\App\Console\IO;
+use CaptainHook\App\Hook\Condition;
+use SebastianFeldmann\Git\Repository;
+
+class YarnDeduplicateInstalled implements Condition
+{
+    public function isTrue(IO $io, Repository $repository): bool
+    {
+        $yarnDeduplicateBin = str_replace("/", DIRECTORY_SEPARATOR, "./node_modules/.bin/yarn-deduplicate");
+        return is_file($yarnDeduplicateBin);
+    }
+}
