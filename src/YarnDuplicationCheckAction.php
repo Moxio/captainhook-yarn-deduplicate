7b007361140ccfce03578c219df27ada48c506f6
Tom den Braber <tom.denbraber@moxio.com>
2021-04-22 16:19:28 +0200

    Accept a Processor as constructor argument

--- src/YarnDuplicationCheckAction.php
+++ src/YarnDuplicationCheckAction.php
@@ -6,11 +6,18 @@ use CaptainHook\App\Config;
 use CaptainHook\App\Console\IO;
 use CaptainHook\App\Exception\ActionFailed;
 use CaptainHook\App\Hook\Action;
+use SebastianFeldmann\Cli\Processor;
 use SebastianFeldmann\Cli\Processor\ProcOpen;
 use SebastianFeldmann\Git\Repository;
 
-class YarnDuplicationCheckAction implements Action
-{
+class YarnDuplicationCheckAction implements Action  {
+    /** @var Processor */
+    private $processor;
+
+    public function __construct(Processor $processor = null) {
+        $this->processor = $processor ?? new ProcOpen();
+    }
+
     public function execute(Config $config, IO $io, Repository $repository, Config\Action $action): void
     {
         $indexOperator = $repository->getIndexOperator();
@@ -18,9 +25,8 @@ class YarnDuplicationCheckAction implements Action
             return;
         }
 
-        $yarnDeduplicateProcess = new ProcOpen();
         $yarnDeduplicateBin = str_replace("/", DIRECTORY_SEPARATOR, "./node_modules/.bin/yarn-deduplicate");
-        $yarnDeduplicateResult = $yarnDeduplicateProcess->run($yarnDeduplicateBin . " --list --fail");
+        $yarnDeduplicateResult = $this->processor->run($yarnDeduplicateBin . " --list --fail");
 
         if ($yarnDeduplicateResult->isSuccessful() === false) {
             if ($yarnDeduplicateResult->getCode() === 1) {
